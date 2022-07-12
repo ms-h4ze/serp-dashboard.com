b47b02664b80579adc516f7d243f727f8d0d8bde
Dolores Haze <d0loresh4ze@gmail.com>
2022-07-13 02:36:40 +0400

    Add example view (prototype)

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SERP Dashboard' });
 });
 
 module.exports = router;
