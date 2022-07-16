0e9a299140489c1fd1994025b33bc3673a393d0b
Dolores Haze <D0loresH4ze@gmail.com>
2022-07-16 07:23:39 +0400

    Style change: Prettier

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cron = require('node-cron');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cron = require("node-cron");
 const axios = require("axios");
-const config = require('config');
-const configPort = config.get('port') || 3001;
+const config = require("config");
+const configPort = config.get("port") || 3001;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const reportsRouter = require('./routes/reports')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const reportsRouter = require("./routes/reports");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/reports', reportsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/reports", reportsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-const checkDashboard = async url => {
+const checkDashboard = async (url) => {
   try {
     await axios.get(url);
     // const response = await axios.get(url);
@@ -55,8 +55,8 @@ const checkDashboard = async url => {
   }
 };
 
-cron.schedule('0 * * * *', () => {
-  console.log('checkDashboard task every hour');
+cron.schedule("0 * * * *", () => {
+  console.log("checkDashboard task every hour");
   let url = `http://127.0.0.1:${configPort}`;
   checkDashboard(url);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,139 +1,138 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require('dotenv').config();
-const SerpApi = require('google-search-results-nodejs');
-const fs = require('fs');
-const config = require('config');
-
+require("dotenv").config();
+const SerpApi = require("google-search-results-nodejs");
+const fs = require("fs");
+const config = require("config");
 
 /* GET home page. */
-router.get('/', async function(req, res) {
+router.get("/", async function (req, res) {
   // brand keywords in format keyword, volume
   const brandKeywords = [
-    ['ベラジョンカジノ', 207000],
-    ['スポーツベットアイオー', 58000],
-    ['カジノシークレット', 56000],
-    ['ミスティーノ', 43000],
-    ['エルドア カジノ', 39000],
-    ['bet365', 33000],
-    ['カジ旅', 30000],
-    ['インターカジノ', 29000],
-    ['コニベット', 28000],
-    ['ビットカジノ', 24000],
-    ['ボンズカジノ', 19000],
-    ['ワンダーカジノ', 17000],
-    ['オンラインカジノ netbet', 16000],
-    ['クイーンカジノ', 15000],
-    ['ユースカジノ', 12000],
-    ['ラッキーニッキー', 12000],
-    ['カジノミー', 10000],
-    ['beebet', 9700],
-    ['ライブカジノハウス', 9700],
-    ['レオベガス', 8600],
-    ['ギャンボラ', 8000],
-    ['10bet', 7700],
-    ['ウィリアムヒル', 6700],
-    ['ピナクル', 6300],
-    ['ジパングカジノ', 6100],
-    ['188bet', 5900],
-    ['チェリーカジノ', 5500],
-    ['カスモカジノ', 4600],
-    ['dora麻雀', 4000],
-    ['アロハシャーク', 4000],
-    ['カジノフライデー', 3900],
-    ['ポーカースターズ', 3800],
-    ['1xbet', 3800],
-    ['コンクエスタドールカジノ', 3600],
-    ['エンパイアカジノ', 3400],
-    ['ビーベット', 3400],
-    ['ベット365', 2900],
-    ['ワイルドジャングルカジノ', 2700],
-    ['ステークカジノ', 2200],
-    ['ユニークカジノ', 2000],
-    ['カジノデイズ', 1700],
-    ['カジノエックス', 1600],
-    ['スロッティベガス', 1400],
-    ['betway', 1400],
-    ['ロイヤルパンダ', 1300],
-    ['ビットスターズ', 1300],
-    ['dafabet', 1200],
-    ['ジョイカジノ', 1200],
-    ['ナショナル カジノ', 1100],
-    ['カジノ イン', 1000],
-    ['ワンバイベット', 1000],
-    ['ロトランド', 1000],
-    ['ビデオスロッツ', 800],
-    ['22bet', 800],
-    ['caxino', 700],
-    ['バルカンベガス', 700],
-    ['フトカジ', 600],
-    ['リリベットカジノ', 600],
-    ['ライブカジノアイオー', 600],
-    ['ポケットカジノ', 600],
-    ['ジャックポットシティ', 500],
-    ['うみうみカジノ', 500],
-    ['プレイアモ', 450],
-    ['ワイルズ カジノ', 450],
-    ['ベットティルト', 450],
-    ['カジノゴッズ', 450],
-    ['1xbit', 450],
-    ['ミラクルカジノ', 450],
-    ['シンプルカジノ', 400],
-    ['ベットランク', 400],
-    ['スピンアウェイカジノ', 350],
-    ['ツインカジノ', 350],
-    ['キングビリーカジノ', 300],
-    ['ノーリミットカジノ', 300],
-    ['ラッキーデイズカジノ', 300],
-    ['ワンダリーノ', 300],
-    ['21.com カジノ', 250],
-    ['マラソンベット', 250],
-    ['スプリーモカジノ', 250],
-    ['バオカジノ', 250],
-    ['ワザンバカジノ', 200],
-    ['カリビアンカジノ', 200],
-    ['ユニバーサルカジノ', 200],
-    ['ピクセルベット', 200],
-    ['エガオンカジノ', 200],
-    ['manekichi', 200],
-    ['スパカジ', 200],
-    ['メタルカジノ', 200],
-    ['cashmio', 150],
-    ['インペリアルカジノ', 150],
-    ['まね吉カジノ', 150],
-    ['ホイールズカジノ', 150],
-    ['ロキカジノ', 150],
-    ['32red', 150],
-    ['ニトロカジノ', 150],
-    ['ハチスロカジノ', 150],
-    ['ベガスプラス', 150],
-    ['betwinner', 150],
-    ['カジニア', 100],
-    ['カシュミオ', 100],
-    ['クラウドベット', 100],
-    ['ロクカジノ', 100],
-    ['ミスター ベガス', 100],
-    ['ラッキー カジノ', 90],
-    ['エムビットカジノ', 90],
-    ['ワンバイビット', 80],
-    ['マネキャッシュ閉鎖', 80],
-    ['ビットキングスカジノ', 80],
-    ['ベットレベルス', 60],
-    ['スロッタムカジノ', 60],
-    ['バスターバンクスカジノ', 50],
-    ['supotsubet', 40],
-    ['21ドットコム', 40],
-    ['21nova', 40],
-    ['ベットウィナー', 30],
-    ['優雅 堂', 20],
-    ['sbo bet', 20],
-    ['モンカジ', 20],
-    ['コインサガ', 10],
-    ['リスクカジノ', 10],
-    ['ロケットポット', 10],
-    ['rocketpot', 10],
-    ];
-  const minVolume = config.get('minVolume');
+    ["ベラジョンカジノ", 207000],
+    ["スポーツベットアイオー", 58000],
+    ["カジノシークレット", 56000],
+    ["ミスティーノ", 43000],
+    ["エルドア カジノ", 39000],
+    ["bet365", 33000],
+    ["カジ旅", 30000],
+    ["インターカジノ", 29000],
+    ["コニベット", 28000],
+    ["ビットカジノ", 24000],
+    ["ボンズカジノ", 19000],
+    ["ワンダーカジノ", 17000],
+    ["オンラインカジノ netbet", 16000],
+    ["クイーンカジノ", 15000],
+    ["ユースカジノ", 12000],
+    ["ラッキーニッキー", 12000],
+    ["カジノミー", 10000],
+    ["beebet", 9700],
+    ["ライブカジノハウス", 9700],
+    ["レオベガス", 8600],
+    ["ギャンボラ", 8000],
+    ["10bet", 7700],
+    ["ウィリアムヒル", 6700],
+    ["ピナクル", 6300],
+    ["ジパングカジノ", 6100],
+    ["188bet", 5900],
+    ["チェリーカジノ", 5500],
+    ["カスモカジノ", 4600],
+    ["dora麻雀", 4000],
+    ["アロハシャーク", 4000],
+    ["カジノフライデー", 3900],
+    ["ポーカースターズ", 3800],
+    ["1xbet", 3800],
+    ["コンクエスタドールカジノ", 3600],
+    ["エンパイアカジノ", 3400],
+    ["ビーベット", 3400],
+    ["ベット365", 2900],
+    ["ワイルドジャングルカジノ", 2700],
+    ["ステークカジノ", 2200],
+    ["ユニークカジノ", 2000],
+    ["カジノデイズ", 1700],
+    ["カジノエックス", 1600],
+    ["スロッティベガス", 1400],
+    ["betway", 1400],
+    ["ロイヤルパンダ", 1300],
+    ["ビットスターズ", 1300],
+    ["dafabet", 1200],
+    ["ジョイカジノ", 1200],
+    ["ナショナル カジノ", 1100],
+    ["カジノ イン", 1000],
+    ["ワンバイベット", 1000],
+    ["ロトランド", 1000],
+    ["ビデオスロッツ", 800],
+    ["22bet", 800],
+    ["caxino", 700],
+    ["バルカンベガス", 700],
+    ["フトカジ", 600],
+    ["リリベットカジノ", 600],
+    ["ライブカジノアイオー", 600],
+    ["ポケットカジノ", 600],
+    ["ジャックポットシティ", 500],
+    ["うみうみカジノ", 500],
+    ["プレイアモ", 450],
+    ["ワイルズ カジノ", 450],
+    ["ベットティルト", 450],
+    ["カジノゴッズ", 450],
+    ["1xbit", 450],
+    ["ミラクルカジノ", 450],
+    ["シンプルカジノ", 400],
+    ["ベットランク", 400],
+    ["スピンアウェイカジノ", 350],
+    ["ツインカジノ", 350],
+    ["キングビリーカジノ", 300],
+    ["ノーリミットカジノ", 300],
+    ["ラッキーデイズカジノ", 300],
+    ["ワンダリーノ", 300],
+    ["21.com カジノ", 250],
+    ["マラソンベット", 250],
+    ["スプリーモカジノ", 250],
+    ["バオカジノ", 250],
+    ["ワザンバカジノ", 200],
+    ["カリビアンカジノ", 200],
+    ["ユニバーサルカジノ", 200],
+    ["ピクセルベット", 200],
+    ["エガオンカジノ", 200],
+    ["manekichi", 200],
+    ["スパカジ", 200],
+    ["メタルカジノ", 200],
+    ["cashmio", 150],
+    ["インペリアルカジノ", 150],
+    ["まね吉カジノ", 150],
+    ["ホイールズカジノ", 150],
+    ["ロキカジノ", 150],
+    ["32red", 150],
+    ["ニトロカジノ", 150],
+    ["ハチスロカジノ", 150],
+    ["ベガスプラス", 150],
+    ["betwinner", 150],
+    ["カジニア", 100],
+    ["カシュミオ", 100],
+    ["クラウドベット", 100],
+    ["ロクカジノ", 100],
+    ["ミスター ベガス", 100],
+    ["ラッキー カジノ", 90],
+    ["エムビットカジノ", 90],
+    ["ワンバイビット", 80],
+    ["マネキャッシュ閉鎖", 80],
+    ["ビットキングスカジノ", 80],
+    ["ベットレベルス", 60],
+    ["スロッタムカジノ", 60],
+    ["バスターバンクスカジノ", 50],
+    ["supotsubet", 40],
+    ["21ドットコム", 40],
+    ["21nova", 40],
+    ["ベットウィナー", 30],
+    ["優雅 堂", 20],
+    ["sbo bet", 20],
+    ["モンカジ", 20],
+    ["コインサガ", 10],
+    ["リスクカジノ", 10],
+    ["ロケットポット", 10],
+    ["rocketpot", 10],
+  ];
+  const minVolume = config.get("minVolume");
   // brand keywords with volume n+
   const testKeywords = brandKeywords.reduce((acc, rec) => {
     if (rec[1] >= minVolume) {
@@ -153,7 +152,7 @@ router.get('/', async function(req, res) {
       gl: "jp",
       hl: "ja",
       num: "100",
-      device: device
+      device: device,
     };
 
     const serpObject = new Promise((resolve) => {
@@ -161,86 +160,121 @@ router.get('/', async function(req, res) {
       search.json(params, (data) => {
         resolve(data);
       });
-    })
+    });
 
     return await serpObject;
-  }
+  };
 
   // today report file name, example: 13072022
-  const todayDate = new Date().toISOString().split('T')[0].split('-');
-  const todayDateFileName = `${todayDate[2]}` + `${todayDate[1]}` + `${todayDate[0]}`
-  const todayDateForReport = `${todayDate[2]}` + '-' + `${todayDate[1]}` + '-' + `${todayDate[0]}`
-  const csvSeparator = ','
-  
+  const todayDate = new Date().toISOString().split("T")[0].split("-");
+  const todayDateFileName =
+    `${todayDate[2]}` + `${todayDate[1]}` + `${todayDate[0]}`;
+  const todayDateForReport =
+    `${todayDate[2]}` + "-" + `${todayDate[1]}` + "-" + `${todayDate[0]}`;
+  const csvSeparator = ",";
+
   // simple crud for reports
   const isTodayReportFileExists = () => {
-    const fileNamesArray = fs.readdirSync('public/reports');
-    return fileNamesArray.includes(todayDateFileName + '.csv');
-  }
+    const fileNamesArray = fs.readdirSync("public/reports");
+    return fileNamesArray.includes(todayDateFileName + ".csv");
+  };
 
   const createTodayReportFile = () => {
-    fs.writeFileSync('public/reports/' + todayDateFileName + '.csv', // `sep=${csvSeparator}\n` +
-      'date' +
-      csvSeparator +
-      'location' +
-      csvSeparator +
-      'search engine' +
-      csvSeparator +
-      'search engine domain' +
-      csvSeparator +
-      'country' + // (ISO 3166-1 alpha-2)
-      csvSeparator +
-      'language' + // (ISO 639-1)
-      csvSeparator +
-      'device' + // (desktop / mobile)
-      csvSeparator +
-      'type of keyword' +
-      csvSeparator +
-      'keyword' +
-      csvSeparator +
-      'keyword volume' +
-      csvSeparator +
-      'domain' +
-      csvSeparator +
-      'exact link' +
-      csvSeparator +
-      'position' +
-      csvSeparator +
-      'title' +
-      csvSeparator +
-      'description');
-    console.log('File ' + todayDateFileName + '.scv' + ' created');
-  }
-
-  const writeToTodayReportFile = (date, location, search_engine, search_engine_domain, country, language, device,
-                                  keyword_type, keyword, keyword_volume, domain, link, position, title, description) =>
-  {
-    fs.appendFileSync('public/reports/' + todayDateFileName + '.csv', '\n'
-      + date + csvSeparator
-      + location + csvSeparator
-      + search_engine + csvSeparator
-      + search_engine_domain + csvSeparator
-      + country + csvSeparator
-      + language + csvSeparator
-      + device + csvSeparator
-      + keyword_type + csvSeparator
-      + keyword.replaceAll(',', '，').replaceAll('"', '”') + csvSeparator
-      + keyword_volume + csvSeparator
-      + domain + csvSeparator
-      + link.replaceAll(',', '，').replaceAll('"', '”') + csvSeparator
-      + position + csvSeparator
-      + title.replaceAll(',', '，').replaceAll('"', '”') + csvSeparator
-      + description.replaceAll(',', '，').replaceAll('"', '”'),
+    fs.writeFileSync(
+      "public/reports/" + todayDateFileName + ".csv", // `sep=${csvSeparator}\n` +
+      "date" +
+        csvSeparator +
+        "location" +
+        csvSeparator +
+        "search engine" +
+        csvSeparator +
+        "search engine domain" +
+        csvSeparator +
+        "country" + // (ISO 3166-1 alpha-2)
+        csvSeparator +
+        "language" + // (ISO 639-1)
+        csvSeparator +
+        "device" + // (desktop / mobile)
+        csvSeparator +
+        "type of keyword" +
+        csvSeparator +
+        "keyword" +
+        csvSeparator +
+        "keyword volume" +
+        csvSeparator +
+        "domain" +
+        csvSeparator +
+        "exact link" +
+        csvSeparator +
+        "position" +
+        csvSeparator +
+        "title" +
+        csvSeparator +
+        "description"
     );
-  }
+    console.log("File " + todayDateFileName + ".scv" + " created");
+  };
 
+  const writeToTodayReportFile = (
+    date,
+    location,
+    search_engine,
+    search_engine_domain,
+    country,
+    language,
+    device,
+    keyword_type,
+    keyword,
+    keyword_volume,
+    domain,
+    link,
+    position,
+    title,
+    description
+  ) => {
+    fs.appendFileSync(
+      "public/reports/" + todayDateFileName + ".csv",
+      "\n" +
+        date +
+        csvSeparator +
+        location +
+        csvSeparator +
+        search_engine +
+        csvSeparator +
+        search_engine_domain +
+        csvSeparator +
+        country +
+        csvSeparator +
+        language +
+        csvSeparator +
+        device +
+        csvSeparator +
+        keyword_type +
+        csvSeparator +
+        keyword.replaceAll(",", "，").replaceAll('"', "”") +
+        csvSeparator +
+        keyword_volume +
+        csvSeparator +
+        domain +
+        csvSeparator +
+        link.replaceAll(",", "，").replaceAll('"', "”") +
+        csvSeparator +
+        position +
+        csvSeparator +
+        title.replaceAll(",", "，").replaceAll('"', "”") +
+        csvSeparator +
+        description.replaceAll(",", "，").replaceAll('"', "”")
+    );
+  };
 
   if (!isTodayReportFileExists()) {
     createTodayReportFile();
 
     // parse desktop SERP
     testKeywords.map(async ([keyword, volume]) => {
-      console.log('working with keyword ' + keyword + ` (${volume}) (desktop serp)`);
+      console.log(
+        "working with keyword " + keyword + ` (${volume}) (desktop serp)`
+      );
       const serpJSON = await getSerpJSON(keyword, "desktop");
       const params = serpJSON.search_parameters;
       const organicResults = serpJSON.organic_results;
@@ -254,21 +288,23 @@ router.get('/', async function(req, res) {
           params.gl, // country (ISO 3166-1 alpha-2)
           params.hl, // language (ISO 639-1)
           params.device, // device (desktop / mobile)
-          'gambling brand', // keyword_type
-          keyword ?? '', // keyword
+          "gambling brand", // keyword_type
+          keyword ?? "", // keyword
           volume, // keyword volume
-          (new URL(result.link)).hostname ?? '', // domain
-          result.link ?? '', // link
+          new URL(result.link).hostname ?? "", // domain
+          result.link ?? "", // link
           result.position, // position
-          result.title ?? '', // title
-          result.snippet ?? '', // description
+          result.title ?? "", // title
+          result.snippet ?? "" // description
         );
       }
     });
 
     // parse mobile SERP
     testKeywords.map(async ([keyword, volume]) => {
-      console.log('working with keyword ' + keyword + ` (${volume}) (mobile serp)`);
+      console.log(
+        "working with keyword " + keyword + ` (${volume}) (mobile serp)`
+      );
       const serpJSON = await getSerpJSON(keyword, "mobile");
       const params = serpJSON.search_parameters;
       const organicResults = serpJSON.organic_results;
@@ -282,23 +318,23 @@ router.get('/', async function(req, res) {
           params.gl, // country (ISO 3166-1 alpha-2)
           params.hl, // language (ISO 639-1)
           params.device, // device (desktop / mobile)
-          'gambling brand', // keyword_type
-          keyword ?? '', // keyword
+          "gambling brand", // keyword_type
+          keyword ?? "", // keyword
           volume, // keyword volume
-          (new URL(result.link)).hostname ?? '', // domain
-          result.link ?? '', // link
+          new URL(result.link).hostname ?? "", // domain
+          result.link ?? "", // link
           result.position, // position
-          result.title ?? '', // title
-          result.snippet ?? '', // description
+          result.title ?? "", // title
+          result.snippet ?? "" // description
         );
       }
     });
-    console.log('Report ' + todayDateFileName + '.csv' + ' created');
+    console.log("Report " + todayDateFileName + ".csv" + " created");
   } else {
-    console.log('Report ' + todayDateFileName + '.csv' + ' exists')
+    console.log("Report " + todayDateFileName + ".csv" + " exists");
   }
 
-  res.render('index', { title: 'SERP Dashboard' });
+  res.render("index", { title: "SERP Dashboard" });
 });
 
 module.exports = router;
